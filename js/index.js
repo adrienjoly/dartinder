02c6b2472e351337188b8339b860b3eff134bca6
Adrien Joly <adrien@whyd.com>
2017-01-14 01:21:39 +0100

    fixed html

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       $(this).addClass('rotate-left').delay(700).fadeOut(1);
       $('.buddy').find('.status').remove();
 
-      $(this).append('<div class="status like">Like!</div>');      
+      $(this).append('<div class="status like">Oh oui!</div>');      
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
        } else {
@@ -15,11 +15,11 @@ $(document).ready(function(){
    $(".buddy").on("swipeleft",function(){
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
-    $(this).append('<div class="status dislike">Dislike!</div>');
+    $(this).append('<div class="status dislike">Non merci!</div>');
 
     if ( $(this).is(':last-child') ) {
      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-      alert('Na-na!');
+      //alert('Na-na!'); // end
      } else {
         $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
     } 
