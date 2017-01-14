635d290176d2ad2ac8fed4c297d598854e1293a1
Adrien Joly <adrien@whyd.com>
2017-01-14 05:29:04 +0100

    sounds

--- js/index.js
+++ js/index.js
@@ -1,28 +1,34 @@
+var honk = new Audio('honk.wav');
+var slap = new Audio('slap.wav');
+
 $(document).ready(function(){
 
-    $(".buddy").on("swiperight",function(){
-      $(this).addClass('rotate-left').delay(700).fadeOut(1);
-      $('.buddy').find('.status').remove();
+  $(".buddy").on("swiperight",function(){
+    $(this).addClass('rotate-left').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
+    $(this).append('<div class="status like">Oh oui!</div>');
+    if ( $(this).is(':last-child') ) {
+      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+    } else {
+      $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+    $('.voiture')
+      .css({ left: '-40%' })
+      .animate({ left: '100%' }, 1000);
+    honk.play();
+  });
 
-      $(this).append('<div class="status like">Oh oui!</div>');      
-      if ( $(this).is(':last-child') ) {
-        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-       } else {
-          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-       }
-    });  
-
-   $(".buddy").on("swipeleft",function(){
+  $(".buddy").on("swipeleft",function(){
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
     $(this).append('<div class="status dislike">Non merci!</div>');
-
     if ( $(this).is(':last-child') ) {
-     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
       //alert('Na-na!'); // end
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    } else {
+      $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
     } 
+    slap.play();
   });
 
 });
